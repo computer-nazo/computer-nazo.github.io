dee3da35ccd2c85696586a97627e57a14a1d6d14
computer-nazo <shumai.sakuhishio@gmail.com>
2026-09-21 17:35:45 +0900

    Create login.js

---
+++ nazo/sadako/homepage/login/login.js
@@ -0,0 +1,10 @@
+function Login() {
+  const UserName = document.getElementById("UserName").value;
+  const UserPwd = document.getElementById("UserPwd").value;
+  const ErrorMsg = document.getElementById("ErrorMsg");
+  if (UserName === "sadako4444" && UserPwd === "sdkadminsadako4444") {
+    window.location.href="https://computer-nazo.github.io/nazo/sadako/homepage/login/correct/admin.html"
+  } else {
+    ErrorMsg.textContent = 'ユーザー名またはパスワードが違います';
+  }
+}
