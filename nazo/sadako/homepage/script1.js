a224885149ab62e84bd74012cb488b5e0713d6da
computer-nazo <shumai.sakuhishio@gmail.com>
2026-09-21 16:40:26 +0900

    Update script1.js

--- nazo/sadako/homepage/script1.js
+++ nazo/sadako/homepage/script1.js
@@ -1,10 +1,10 @@
 function Search() {
   const SearchBox = document.getElementById("SearchBox").value;
   if (SearchBox === "keyword") {
-    window.location.href="../search/keyword.html";
+    window.location.href="https://computer-nazo.github.io/nazo/sadako/homepage/search/keyword.html";
   } else if (SearchBox === "122573") {
-    window.location.href="../search/122573.html";
+    window.location.href="https://computer-nazo.github.io/nazo/sadako/homepage/search/122573.html";
   } else {
-    window.location.href="../search/keywordnotfound.html";
+    window.location.href="https://computer-nazo.github.io/nazo/sadako/homepage/search/keywordnotfound.html";
   }
 }
