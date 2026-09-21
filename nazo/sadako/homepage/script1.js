eabd1ccf5e3083bd9b1b4bfb221b1e2c06810204
computer-nazo <shumai.sakuhishio@gmail.com>
2026-09-21 17:41:21 +0900

    Update script1.js

--- nazo/sadako/homepage/script1.js
+++ nazo/sadako/homepage/script1.js
@@ -1,5 +1,5 @@
 const SideBar = document.getElementById("SideBar");
-SideBar.innerHTML = '<a href="https://computer-nazo.github.io/nazo/sadako/homepage/index.html">ホームページ</a>';
+SideBar.innerHTML = '<a href="https://computer-nazo.github.io/nazo/sadako/homepage/index.html">ホームページ</a><br><a href="https://computer-nazo.github.io/nazo/sadako/homepage/login/index.html"></a>';
 
 function Search() {
   const SearchBox = document.getElementById("SearchBox").value;
