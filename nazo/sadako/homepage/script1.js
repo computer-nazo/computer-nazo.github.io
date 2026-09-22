acfcc1f59c839d16e0f1c07af4cbe68e76619927
computer-nazo <shumai.sakuhishio@gmail.com>
2026-09-22 19:01:02 +0900

    Update script1.js

--- nazo/sadako/homepage/script1.js
+++ nazo/sadako/homepage/script1.js
@@ -1,5 +1,5 @@
 const SideBar = document.getElementById("SideBar");
-SideBar.innerHTML = '<a href="https://computer-nazo.github.io/nazo/sadako/homepage/index.html">ホームページ</a><br><a href="https://computer-nazo.github.io/nazo/sadako/homepage/login/index.html">ログイン</a>';
+SideBar.innerHTML = '<a href="https://computer-nazo.github.io/nazo/sadako/homepage/index.html">ホームページ</a><br><a href="https://computer-nazo.github.io/nazo/sadako/homepage/login/index.html">ログイン</a><br><h3>ゲーム内操作</h3><br><a href="https://computer-nazo.github.io/nazo/sadako/homepage/win95.html>windows95のuiで遊ぶ（試験的機能）</a>">';
 
 function Search() {
   const SearchBox = document.getElementById("SearchBox").value;
